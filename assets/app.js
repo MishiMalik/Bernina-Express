f4172870e6de4cfd2150b81ae0b49a86ffaee407
Mehwish <mehwishgul715@gmail.com>
2023-02-01 21:07:08 +0500

    commit

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,49 @@
+// ===================================header
 $(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll > 100) {
-          $(".navbar").css("background-color" , "#d32f2f");
-        }
-  
-        else{
-            $(".navbar").css("background-color" , "rgba(0, 0, 0, 0.237)");  	
-        }
-    })
-    $('.navbar-toggler').on('click', function(){
-        $('.navbar').toggleClass('color-changed');
-    });
+  $(window).scroll(function(){
+      var scroll = $(window).scrollTop();
+      if (scroll > 100) {
+        $(".navbar").css("background-color" , "#d32f2f");
+      }
+
+      else{
+          $(".navbar").css("background-color" , "rgba(0, 0, 0, 0.237)");  	
+      }
   })
+  $('.navbar-toggler').on('click', function(){
+      $('.navbar').toggleClass('color-changed');
+  });
+})
+
+$(".show-date").on("click",function(){
+  $(".display-more").css("display","flex");
+})
+// =====================select2
+$(document).ready(function() {
+  $('.js-example-basic-single').select2();
+});
+
+// ====================================inc/dec
+const plus = document.querySelector(".plus"),
+minus = document.querySelector(".minus"),
+num = document.querySelector(".num");
+let a = 1;
+plus.addEventListener("click", ()=>{
+  a++;
+  a = (a < 10) ? "0" + a : a;
+  num.innerText = a;
+});
+
+minus.addEventListener("click", ()=>{
+  if(a > 1){
+    a--;
+    a = (a < 10) ? "0" + a : a;
+    num.innerText = a;
+  }
+});
+
+
+
 //  ====================================search input
 
 //  $('.search-btn').on('click', function(){
